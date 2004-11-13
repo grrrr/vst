88679b5d6ac986a6a98cf2cba2374fa39ddb2005
Thomas Grill <gr@grrrr.org>
2004-11-13 16:10:20 +0000

    editor-less Mac version
    git-svn-id: https://svn.grrrr.org/ext/vst/trunk@3061 4d9ac71a-51e6-0310-8455-cad1006bcd31

---
+++ src/EditorMac.cpp
@@ -0,0 +1,55 @@
+/* 
+vst~ - VST plugin object for PD 
+based on the work of Jarno Seppänen and Mark Williamson
+
+Copyright (c)2003-2004 Thomas Grill (xovo@gmx.net)
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "license.txt," in this distribution.  
+*/
+
+#include "Editor.h"
+#include "VstHost.h"
+#include <flext.h>
+
+
+#if FLEXT_OS == FLEXT_OS_MAC
+// only Mac OSX code is situated in this file
+
+void SetupEditor()
+{
+}
+
+void StartEditor(VSTPlugin *p)
+{
+}
+
+void StopEditor(VSTPlugin *p) 
+{
+}
+
+void ShowEditor(VSTPlugin *p,bool show) 
+{
+}
+
+void MoveEditor(VSTPlugin *p,int x,int y) 
+{
+}
+
+void SizeEditor(VSTPlugin *p,int x,int y) 
+{
+}
+
+void CaptionEditor(VSTPlugin *plug,bool c)
+{
+}
+
+void TitleEditor(VSTPlugin *p,const char *t) 
+{
+}
+
+bool IsEditorShown(const VSTPlugin *p) 
+{
+    return false;
+}
+
+#endif // FLEXT_OS_MAC
